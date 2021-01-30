a117636b038b96c2198591d57c408c2c6c8b6ffe
Anjelica <>
2021-01-29 18:28:25 -0800

    express, handlebars init

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 const express = require('express')
 const app = express()
 
-// Tell our app to send the "hello world" message to our home page
+//handlebars
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+
+app.engine('handlebars', hbs.engine); 
+app.set('view engine', 'handlebars');
+
+// Render the "home" layout for the main page and send the following msg
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home', { msg: 'Handlebars are Cool!' });
 })
 
 // Choose a port to listen on
